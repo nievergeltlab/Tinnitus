9bc51ab7eec8b5fbbc29a6205772de705da83f38
Adam X. Maihofer <amaihofer@ucsd.edu>
2023-11-03 14:29:10 -0700

    Update 01_credible_sets.sh

--- Fine-Mapping (Polyfun)/01_credible_sets.sh
+++ Fine-Mapping (Polyfun)/01_credible_sets.sh
@@ -1,4 +1,4 @@
-
+#Read polyfun output files (concatenated files)
 d1 <- read.table('finemapLD.inform.TRANS.complete.txt',stringsAsFactors=F,header=T)
 
 library(plyr)
@@ -10,28 +10,26 @@ r1 <- range(x)
 return(c(r1[1], r1[2]))
 }
 
-credsets1 <- ddply(d1, ~ Locus, colwise(min,"BP"))
-credsets2 <- ddply(d1, ~ Locus, colwise(max,"BP"))
-
-
-write.table(cbind(credsets1,credsets2),file="credible_set_windows.txt",row.names=F)
-
-#How many SNPs in each set?
-
-credsets2 <- ddply(d1, ~ Locus, colwise(length,"BP"))
-
-write.table(credsets2,file="credible_set_nsnps.txt",row.names=F)
-
-#How many snps testsed?
-
+#Count how many SNPs were tested (i.e. this will include the SNPs in the credible set and those that do not make it to the credible set)
 d2 <- read.table('finemapLD.inform.EUR.complete.allsnps.txt',stringsAsFactors=F,header=T)
 credsets3 <- ddply(d2, ~ Locus, colwise(length,"BP"))
 
 write.table(credsets3,file="all_set_nsnps.txt",row.names=F)
 
+#Calculate width of credible set
+credsets1 <- ddply(d1, ~ Locus, colwise(min,"BP"))
+credsets2 <- ddply(d1, ~ Locus, colwise(max,"BP"))
 
-#Get the N SNPs within the set for FUMA as well (interset with the list put into the program)
+write.table(cbind(credsets1,credsets2),file="credible_set_windows.txt",row.names=F)
 
+#Count how many SNPs are in credible set
+credsets2 <- ddply(d1, ~ Locus, colwise(length,"BP"))
+
+write.table(credsets2,file="credible_set_nsnps.txt",row.names=F)
+
+
+
+#Merge FUMA snps.txt with the SNP results from Polyfun (i.e. to annotate SNPs in credible sets)
 d3 <- fread('eur_snps.txt',data.table=F)
 d3$SNP <- d3$rsID
 
